3b90a86160bc8fb920455f1446e5f69000659809
ya4615 <ya4615@naver.com>
2017-12-15 01:17:02 +0900

    data loader

--- Question2.py
+++ Question2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 
 class DataLoader():
@@ -8,13 +9,29 @@ class DataLoader():
     def __init__(self):
         pass
 
-    def read_data_(self, file_path_):
-        df = pd.read_csv(file_path_)
-        print(df.ix[:, 'St':'Vt1'])
+    def read_data_(self, file_path_, file_name_):
+        assert file_name_.split('.')[-1] == "csv", 'Invalid file format.'
 
+        df = pd.read_csv(file_path_ + "\\" + file_name_)
+        # print(type(df.ix[:, 'St':'Vt1']))
+        # print df.ix[1, 'St':'Vt1']
 
+        label = df.ix[:, 'St'].values
+        data  = df.ix[:, 'EMAt1': 'CMFt2'].values
 
-if __name__=="__main__":
+        return data, label
 
+if __name__ == "__main__":
+
+    data_path_ = "./deepae_Dataset(tr,te)"
     dt = DataLoader()
-    dt.read_data_("D:\\workspace\\github\\machine_learning_homework\\deepae_Dataset(tr,te)\\cv1_te.csv")
+
+    for i in range(0,10):
+        tr_file_name_ = "cv%d_tr.csv" %i
+        te_file_name_ = "cv%d_te.csv" %i
+
+        train_data , train_label = dt.read_data_(data_path_, tr_file_name_)
+        test_data, test_label = dt.read_data_(data_path_, te_file_name_)
+
+
+
