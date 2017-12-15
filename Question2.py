b015630e6dd496295951c007f84b228d1bfebb5f
ya4615 <ya4615@naver.com>
2017-12-15 14:23:22 +0900

    question 2

--- Question2.py
+++ Question2.py
@@ -25,7 +25,25 @@ class DataLoader():
         return data, label
 
 
+def normalize(data_, label_, norm_relate_):
+
+    data = (data_ - norm_relate_.ix['min', 'EMAt1': 'CMFt2']) / \
+           (norm_relate_.ix['max', 'EMAt1': 'CMFt2'] - norm_relate_.ix['min', 'EMAt1': 'CMFt2'])
+
+    label = (label_ - norm_relate_.ix['min', 'St']) / \
+            (norm_relate_['max', 'EMAt1': 'CMFt2'] - norm_relate_['min', 'EMAt1': 'CMFt2'])
+
+    return data, label
+
+
+def de_normalize(norm_label_, norm_relate_):
+
+    return norm_label_ * (norm_relate_.ix['max', 'St'] - norm_relate_.ix['min', 'St']) + norm_relate_.ix['min', 'St']
+
 if __name__ == "__main__":
+
+    norm_path_ = "./KospiData.xlsx"
+    min_max_ = pd.read_excel(norm_path_)
 
     data_path_ = "./deepae_Dataset(tr,te)"
     dt = DataLoader()
@@ -37,28 +55,41 @@ if __name__ == "__main__":
     dnn_dict = {"MAE"    : [],
                 "RMSE"   : [],
                 "CVRMES" : []}
-    for i in range(1,11):
-        tr_file_name_ = "cv%d_tr.csv" %i
-        te_file_name_ = "cv%d_te.csv" %i
 
-        train_data , train_label = dt.read_data_(data_path_, tr_file_name_)
+    for i in range(1, 11):
+        tr_file_name_ = "cv%d_tr.csv" % i
+        te_file_name_ = "cv%d_te.csv" % i
+
+        train_data, train_label = dt.read_data_(data_path_, tr_file_name_)
         test_data, test_label = dt.read_data_(data_path_, te_file_name_)
+
+        #############################################
+        #                 normalize                 #
+        #############################################
+        norm_train_data, norm_train_label = normalize(train_data, train_label, min_max_)
+        norm_test_data, norm_test_label = normalize(test_data, test_data, min_max_)
 
         #############################################
         #                  train                    #
         #############################################
-        svr = SVR().fit(train_data, train_label)
-        dnn = MLPRegressor().fit(train_data, train_label)
+        svr = SVR().fit(norm_train_data, norm_train_label)
+        dnn = MLPRegressor().fit(norm_train_data, norm_train_label)
 
         #############################################
         #                  predict                  #
         #############################################
-        svr_result = svr.predict(test_data)
-        dnn_result = dnn.predict(test_data)
+        svr_result = svr.predict(norm_test_data)
+        dnn_result = dnn.predict(norm_test_data)
+
+        #############################################
+        #            de-normalize result            #
+        #############################################
+        de_norm_svr_result = de_normalize(svr_result, min_max_)
+        de_norm_dnn_result = de_normalize(dnn_result, min_max_)
 
         n = len(test_label)
-        svr_disparity = test_label - svr_result
-        dnn_disparity = test_label - dnn_result
+        svr_disparity = test_label - de_norm_svr_result
+        dnn_disparity = test_label - de_norm_dnn_result
 
         svr_MAE = sum(map(abs, svr_disparity)) / n
         dnn_MAE = sum(map(abs, dnn_disparity)) / n
