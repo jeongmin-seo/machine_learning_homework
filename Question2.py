2ce55127d2a9f6eba8eca17bbb9de750aadac9de
ya4615 <ya4615@naver.com>
2017-12-15 13:22:43 +0900

    question 2

--- Question2.py
+++ Question2.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+from sklearn.neural_network import MLPRegressor
+from sklearn.svm import SVR
 
 
 class DataLoader():
@@ -9,7 +11,8 @@ class DataLoader():
     def __init__(self):
         pass
 
-    def read_data_(self, file_path_, file_name_):
+    @staticmethod
+    def read_data_(file_path_, file_name_):
         assert file_name_.split('.')[-1] == "csv", 'Invalid file format.'
 
         df = pd.read_csv(file_path_ + "\\" + file_name_)
@@ -21,17 +24,56 @@ class DataLoader():
 
         return data, label
 
+
 if __name__ == "__main__":
 
     data_path_ = "./deepae_Dataset(tr,te)"
     dt = DataLoader()
 
-    for i in range(0,10):
+    svr_dict = {"MAE"    : [],
+                "RMSE"   : [],
+                "CVRMES" : []}
+
+    dnn_dict = {"MAE"    : [],
+                "RMSE"   : [],
+                "CVRMES" : []}
+    for i in range(1,11):
         tr_file_name_ = "cv%d_tr.csv" %i
         te_file_name_ = "cv%d_te.csv" %i
 
         train_data , train_label = dt.read_data_(data_path_, tr_file_name_)
         test_data, test_label = dt.read_data_(data_path_, te_file_name_)
+
+        #############################################
+        #                  train                    #
+        #############################################
+        svr = SVR().fit(train_data, train_label)
+        dnn = MLPRegressor().fit(train_data, train_label)
+
+        #############################################
+        #                  predict                  #
+        #############################################
+        svr_result = svr.predict(test_data)
+        dnn_result = dnn.predict(test_data)
+
+        n = len(test_label)
+        svr_disparity = test_label - svr_result
+        dnn_disparity = test_label - dnn_result
+
+        svr_MAE = sum(map(abs, svr_disparity)) / n
+        dnn_MAE = sum(map(abs, dnn_disparity)) / n
+        svr_dict["MAE"].append(svr_MAE)
+        dnn_dict["MAE"].append(dnn_MAE)
+
+        svr_RMSE = np.sqrt(sum(map(pow, svr_disparity)) / n)
+        dnn_RMSE = np.sqrt(sum(map(pow, dnn_disparity)) / n)
+        svr_dict["RMSE"].append(svr_RMSE)
+        dnn_dict["RMSE"].append(dnn_RMSE)
+
+        svr_CVRMSE = svr_RMSE / sum(test_label) / n
+        dnn_CVRMSE = dnn_RMSE / sum(test_label) / n
+        svr_dict["CVRMSE"].append(svr_CVRMSE)
+        dnn_dict["CVRMSE"].append(dnn_CVRMSE)
 
 
 
